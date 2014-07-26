edfe8b68b3535755a7f1ea60639f50f011e798ca
Sean Talts <sean.talts@gmail.com>
2014-07-26 10:54:31 -0400

    Whoa.

--- Main.hs
+++ Main.hs
@@ -5,7 +5,7 @@ import Web.Scotty
 import Control.Monad.IO.Class  (liftIO)
 import Models
 import Utils
-import Database.Persist.Sqlite (runMigration)
+import Database.Persist.Sqlite (runMigration, insert)
 import Control.Applicative
 import Data.Aeson (eitherDecode)
 import qualified Data.ByteString.Lazy as B
@@ -18,8 +18,6 @@ main = do
     get "/users" $ do
       json User {userName="Sean", userEmail="sean@sean", userPhone="434.713.0499"}
     post "/users" $ do
-      b <- (eitherDecode <$> body :: IO B.ByteString) :: IO (Either String [User])
-      case b of
-        Left err -> putStrLn err
-        Right ps -> print ps
-      
+      u <- jsonData :: ActionM User
+      liftIO $ runDb $ insert u
+      json $ u
