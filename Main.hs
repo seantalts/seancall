76fbf6261cee9c6bee7c113c0b7557fd94da137d
Sean Talts <sean.talts@gmail.com>
2014-07-26 12:11:57 -0400

    twilio lib

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 
-import Web.Scotty
+import Web.Scotty as S
 import Control.Monad.IO.Class  (liftIO)
 import Models
 import Utils
@@ -9,16 +9,18 @@ import Database.Persist.Sqlite (runMigration, insert)
 import Control.Applicative
 import Data.Aeson (eitherDecode)
 import qualified Data.ByteString.Lazy as B
+import Twilio.Calls as Calls
+import Twilio.Types as TwilTypes
 
 main = do
   runDb $ runMigration migrateAll
   scotty 3000 $ do
-    get "/" $
+    S.get "/" $
       json [(0::Int)..10]
-    get "/users" $ do
+    S.get "/users" $ do
       _users <- liftIO readUsers
       json _users
-    post "/users" $ do
+    S.post "/users" $ do
       u <- jsonData :: ActionM User
       liftIO $ runDb $ insert u
       json u
