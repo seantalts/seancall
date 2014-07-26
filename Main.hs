e8bbb8bbc048d729669d5f90900d3686957cc49c
Sean Talts <sean.talts@gmail.com>
2014-07-25 23:12:24 -0400

    simpler

--- Main.hs
+++ Main.hs
@@ -1,15 +1,6 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+import Web.Scotty
 
-import           Control.Applicative                  ((<$>))
-import           Controllers.Home                     (home, login)
-import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import           Network.Wai.Middleware.Static        (addBase, noDots,
-                                                       staticPolicy, (>->))
-import           System.Environment                   (getEnv)
-import           Web.Scotty                           (middleware, scotty)
-
-main :: IO ()
-main = do
-  port <- read <$> getEnv "PORT"
-  scotty port
-    get "/" json "Hello world!"
+main = scotty 3000 $ do
+  get "/" $ do
+    html "Hello World!"
