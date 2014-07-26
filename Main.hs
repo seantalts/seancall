6d5c4162f4bb07170acf1ac3ac0ffae31e114753
Sean Talts <sean.talts@gmail.com>
2014-07-25 22:23:12 -0400

    Starter app time

--- Main.hs
+++ Main.hs
@@ -1,22 +1,19 @@
-import Control.Monad
-import Data.Char
-import System.IO
-import System.IO.Error
-import Network
-import Network.BSD
-import Network.Socket
-import System.Environment
-import System.Process
+module Main where
 
-main = do [host,port] <- getArgs
-          sock <- socket AF_INET Stream defaultProtocol
-          setSocketOption sock ReuseAddr 1
-          inet <- inet_addr host
-          bind sock $ SockAddrInet (fromInteger $ read port) inet
-          listen sock maxListenQueue
-          packages <- readProcess "ghc-pkg" ["list", "--simple-output"] []
-          let response = "HTTP/1.1 200 OK\r\n\r\nWelcome to Haskell Cloud! The following packages are pre-installed:\n\n" ++ unlines (words packages)
-          forever $ do (handle,_,_) <- Network.accept sock
-                       read <- liftM (any (null . dropWhile isSpace) . lines) $ hGetContents handle
-                       when read $ void $ tryIOError $ hPutStr handle response
-                       hClose handle
+import           Control.Applicative                  ((<$>))
+import           Controllers.Home                     (home, login)
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Network.Wai.Middleware.Static        (addBase, noDots,
+                                                       staticPolicy, (>->))
+import           System.Environment                   (getEnv)
+import           Web.Scotty                           (middleware, scotty)
+
+main :: IO ()
+main = do
+  port <- read <$> getEnv "PORT"
+  scotty port $ do
+         middleware $ staticPolicy (noDots >-> addBase "static/images") -- for favicon.ico
+         middleware logStdoutDev
+         home >> login
+
+
